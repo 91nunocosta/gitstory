5ab521451f4dee025a43eb8421da6a4c36a1c704
nunocosta <91nunocosta@gmail.com>
2019-02-23 12:30:14 +0000

    library to obtain git history from cli sketched

---
+++ pygitstory/gitlog.py
@@ -0,0 +1,16 @@
+"""Git commit histories."""
+
+class GitLog:
+    """A git commit history."""
+
+    def __init__(self, commits=[]):
+        """Initialize a git commit history."""
+        pass
+
+class GitCommit:
+    """A git commit."""
+
+    def __init__(self, sha, author_date, author_name, author_email, 
+                 commiter_date, commiter_name, commiter_email, message):
+        """Initialize a commit."""
+        pass
---
+++ pygitstory/gitrepo.py
@@ -0,0 +1,27 @@
+"""Git repositories."""
+
+class GitRepo:
+    """A git repository."""
+
+    def __init__(self, remote_url, path=None):
+        """
+        Initialize a git repository for a given remote repository url.
+        
+        Arguments:
+        remote_url -- URI of the remote repository.
+        path -- the path for the directory where the repository was or will be cloned.
+                Defaults to current working directory.
+        """
+        pass
+
+    def clone(self):
+        """
+        Clone a git repository.
+
+        Do nothing if the repository was already clone into the specified path.
+        """
+        pass
+    
+    def log(self):
+        """Get the repository commit history."""
+        return None
