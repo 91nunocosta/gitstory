8c19725bc01e8d2196c57cbeb1cf80cc0ba69ab9
nunocosta <91nunocosta@gmail.com>
2019-02-24 21:09:41 +0000

    fix gitlogstore tests

--- pygitstory/tests/test_gitlogstore.py
+++ pygitstory/tests/test_gitlogstore.py
@@ -15,48 +15,48 @@ TARGET_REPO = 'https://github.com/example/target-repo.git'
 OTHER_REPO = 'https://github.com/example/other-repo.git'
 COMMITS = [
     {
-        "repo_url": TARGET_REPO,
+        REPO_URL_FIELD: TARGET_REPO,
         "sha": "15c8c55ea88e9d2e3b2f975cc8eef30848080b7f",
-        "author_date": "2018-06-08 15:56:25",
+        "author_date": "2018-06-08 13:56:25Z",
         "author_name": "Johann Egger",
         "author_email": "johann@codacy.com",
-        "commiter_date": "2018-06-08 15:56:25",
+        "commiter_date": "2018-06-08 13:56:25Z",
         "commiter_name": "Johann Egger",
         "commiter_email": "johann@codacy.com",
         "message": "first commit"
     },
     {
-        "repo_url": TARGET_REPO,
+        REPO_URL_FIELD: TARGET_REPO,
         "sha": "00a5762b09a7704aef6e30ccd7c368758a56261a",
-        "author_date": "2018-06-08 15:59:55",
+        "author_date": "2018-06-08 13:59:55Z",
         "author_name": "Johann Egger",
         "author_email": "johann@codacy.com",
-        "commiter_date": "2018-06-08 15:59:55",
+        "commiter_date": "2018-06-08 13:59:55Z",
         "commiter_name": "Johann Egger",
         "commiter_email": "johann@codacy.com",
         "message": "Add a file"
     },
     {
-        "repo_url": TARGET_REPO,
+        REPO_URL_FIELD: TARGET_REPO,
         "sha": "57012cdf0c3b277ba2feef552ab60d58f13bf774",
-        "author_date": "2018-06-08 16:00:27",
+        "author_date": "2018-06-08 14:00:27Z",
         "author_name": "Johann Egger",
         "author_email": "johann@codacy.com",
-        "commiter_date": "2018-06-08 16:00:27",
+        "commiter_date": "2018-06-08 14:00:27Z",
         "commiter_name": "Johann Egger",
         "commiter_email": "johann@codacy.com",
         "message": "Rename a file"
     },
     {
-        "repo_url": OTHER_REPO,
-        "sha": "57012cdf0c3b277ba2feef552ab60d58f13bf774",
-        "author_date": "2018-06-08 16:00:27",
+        REPO_URL_FIELD: OTHER_REPO,
+        "sha": "5e013cdf0c3bds77ba4feef552af60d58f13bf774",
+        "author_date": "2018-06-08 14:00:27Z",
         "author_name": "Johann Egger",
         "author_email": "johann@codacy.com",
-        "commiter_date": "2018-06-08 16:00:27",
+        "commiter_date": "2018-06-08 14:00:27Z",
         "commiter_name": "Johann Egger",
         "commiter_email": "johann@codacy.com",
-        "message": "Rename a file"
+        "message": "Something"
     }
 ]
 
@@ -77,7 +77,8 @@ class TestMongoGitlogStore(TestCase):
 
     def test_put(self):
         self.gitlog_store.put(TARGET_REPO, GitLog(GIT_TEST_LOG))
-        found_commits = list(self.gitlog_store.commits.find({REPO_URL_FIELD: TARGET_REPO}))
+        found_commits = list(self.gitlog_store.commits.find(
+            {REPO_URL_FIELD: TARGET_REPO}))
         for commit in found_commits:
             # _id is a Mongo added key
             del commit['_id']
@@ -86,8 +87,8 @@ class TestMongoGitlogStore(TestCase):
         expected_commits = deepcopy(COMMITS[:3])
         for commit in expected_commits:
             # date fields are stored in Mongo as strings not datetime
-            commit['author_date'] = parse(commit['author_date'])
-            commit['commiter_date'] = parse(commit['commiter_date'])
+            commit['author_date'] = parse(commit['author_date']).replace(tzinfo=None)
+            commit['commiter_date'] = parse(commit['commiter_date']).replace(tzinfo=None)
         self.assertListEqual(found_commits, expected_commits)
 
     def test_has(self):
