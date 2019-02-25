80cca0dba1b95b3a27f4fcec597be8e828297b81
nunocosta <91nunocosta@gmail.com>
2019-02-25 00:44:17 +0000

    updates gitstory script to handle exceptions

--- pygitstory/gitstory.py
+++ pygitstory/gitstory.py
@@ -1,5 +1,7 @@
 from pygitstory.github import Github
 from pygitstory.gitlog_store import MongoGitlogStore
+from pygitstory.exceptions import *
+
 import os
 from argparse import ArgumentParser
 
@@ -16,9 +18,21 @@ def execute():
         repos_dir = os.environ['REPOS_DIR']
     except KeyError as e:
         argparser.error('Please set {} environment variable.'.format(str(e)))
-    gitlog_store = MongoGitlogStore(mongo_host, mongo_port, mongo_db)
-    github = Github(repos_dir=repos_dir, gitlog_store=gitlog_store)
-    history = github.get_history(args.url)
+    try:
+        gitlog_store = MongoGitlogStore(mongo_host, mongo_port, mongo_db)
+        github = Github(repos_dir=repos_dir, gitlog_store=gitlog_store)
+        history = github.get_history(args.url)
+    except Exception as err:
+        if isinstance(err, RepoNotFound):
+            argparser.error('Sorry, the repository does not exist.')
+        elif isinstance(err, InvalidGitHost):
+            argparser.error('Sorry, the repository url has an invalid host.')
+        elif isinstance(err, InvalidReposDirectory):
+            argparser.error('Sorry, the configured REPOS_DIR is not accessible.')
+        else:
+            argparser.error("Ups, I don't now what happened.")
+            argparser.error("Blame the developer and forward him the following trace:")
+            argparser.error(str(err))
     for commit in history.commits:
         row = [str(e) for e in commit.__dict__.values()]
         print('\t'.join(row))
